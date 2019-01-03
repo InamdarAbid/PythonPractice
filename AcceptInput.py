aa446303a07b3cf70ae3d3c94a9e223c3cc0e48e
InamdarAbid <2015abid.inamdar@ves.ac.in>
2019-01-03 16:37:01 +0530

    String Formating and accepting input

---
+++ AcceptInput.py
@@ -0,0 +1,14 @@
+'''name = input("Input name :")
+con = input("Input your Country :")
+print("Your name is",name,"and you live in",con)'''
+
+#Area of circle
+rad = int(input("Enter radius : "))
+area = 3.14* rad**2
+print("Area of circle is {0:.4f}".format(area))
+
+n1 = 1245
+n2 = 1548
+
+print('The two numbers are {0:.3f} and {1:.4f}'.format(n1,n2)) #if you need formatting
+print(f'The two numbers are{n1} and {n2}')
