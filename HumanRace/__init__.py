13f6637368d5b7fea15b0aa031bf969046c55603
InamdarAbid <2015abid.inamdar@ves.ac.in>
2019-01-06 19:23:30 +0530

    Added documentation

---
+++ HumanRace/__init__.py
@@ -0,0 +1,2 @@
+#This file is a must for python to understand this is a package.
+#It's ok to keep this file blank if required.
--- ModulesAndPackages.py
+++ ModulesAndPackages.py
@@ -4,10 +4,14 @@
 # person = Names()
 # person.display() #You can also use print statement here
 # print(person.name,"is",person.height,"ft tall")
-
-from HumanRace.Human import Names
+'''
+For understanding  modules uncomment above code and comment below code.
+For Packages understanding downloading of HumanRace folder is necessary.
+HumanRace is a package and in that package we have Human and calc as packages.
+'''
+from HumanRace.Human import Names 
 from HumanRace.calc import human_age,area
 
-person1 = Names("Abid",22,5.5,"i10")
-print(human_age(person1.age))
+person1 = Names("Abid",22,5.5,"i10") #Initialise object
+print(human_age(person1.age))  
 print(area(15))
