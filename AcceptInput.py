2373e7cd23d9523be5bdf7feb998288e2e6eb9e6
InamdarAbid <2015abid.inamdar@ves.ac.in>
2019-02-01 08:59:30 +0530

    Fixed some typos

--- AcceptInput.py
+++ AcceptInput.py
@@ -7,8 +7,8 @@ rad = int(input("Enter radius : "))
 area = 3.14* rad**2
 print("Area of circle is {0:.4f}".format(area))
 
-n1 = 1245
-n2 = 1548
+n1 = 1245.1545512
+n2 = 1548.368741
 
 print('The two numbers are {0:.3f} and {1:.4f}'.format(n1,n2)) #if you need formatting
-print(f'The two numbers are{n1} and {n2}')
+print(f'The two numbers are{n1:.3f} and {n2:.3f}')
--- Strings.py
+++ Strings.py
@@ -3,7 +3,7 @@ print('Hello')
 print("He's a stupid")
 
 #print('he's a stupid' ) Gives error
-print('he\'s a stupid') #Escape character
+print('he\'s a stupid person.') #Escape character
 
 a1 = 'Hello'
 #Important for competative where new line output not required
@@ -18,7 +18,7 @@ print(a1[-4:-1]) #ell
 print(a1[-1:3]) #empty string
 print(a1[-1:-3]) #empty string
 print(a1[2:-1])
-print(a1) #Its unchanged untill reassigned
+print(a1) #It's unchanged until reassigned
 a2 = 'world'
 a3 = a1 + a2 #String concatination
 print(a3)
@@ -32,7 +32,7 @@ print(a3.lower())
 friends = "Aman,Vishal,Sagar"
 print(friends.split(","))
 print(len(a3))
-string = "good morning"
+string = "goodmorning"
 print(string.capitalize()) #Capitalize first lettor of string
 print(string.center(1)) #Returns a centered string
 print(string.count("hello")) #Returns the number of times a specified value occurs in a string
@@ -52,3 +52,6 @@ print(my_string)
 my_string = """Hello, welcome to
            the world of Python"""
 print(my_string)
+my_string = "Welcome To My Wolrd"
+print(my_string.istitle())
+print(my_string.isprintable())
