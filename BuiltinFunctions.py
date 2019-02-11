4363cf67e636f626446c59addfcbb9efe49008ec
InamdarAbid <2015abid.inamdar@ves.ac.in>
2019-02-11 07:34:14 +0530

    Builtin Functions

---
+++ BuiltinFunctions.py
@@ -0,0 +1,15 @@
+print(abs(-58))
+if 58 == abs(-58):
+    print("Same")
+
+#help function
+'''
+Type the name of function you need help of.
+Exe
+'''
+# print(help('time'))
+list1 = [10,45,7,5,1,5,8,84,85,7845,96,4,35,95,45]
+print(max(list1))
+print(min(list1))
+y = 12.58
+print(round(y))
